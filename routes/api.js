02b94adc9b66df40fd1fb260b83a874ebd406c56
Muhammad Febriyan Akbar <muh.febrian@gmail.com>
2023-09-02 23:07:15 +0700

    Comment

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,7 @@ router.get("/login", (req, res) => {
   res.json({ items: [] });
 });
 
+//Route
 router.get("/master/category", MasterController.index);
 router.post("/master/category/add", MasterController.store);
 router.post("/master/category/get", MasterController.getData);
